b21880224e93ac6a2225c397d1bdf771efc3fc54
TheMaximeSan <mxm.tournier@gmail.com>
2021-08-03 10:07:18 +0200

    create and delete

--- src/Controller/ShortController.php
+++ src/Controller/ShortController.php
@@ -4,9 +4,12 @@ namespace App\Controller;
 
 use App\Entity\Short;
 use App\Repository\ShortRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
 
 
 /**
@@ -25,10 +28,37 @@ class ShortController extends AbstractController
     }
 
     /**
-     * @Route("/{id}", name="shortShow")
+     * @Route("/{id}", name="shortShow", requirements={"id":"\d+"})
      */
     public function show(Short $short): Response
     {
         return $this->json($short);
     }
+
+    /**
+     * @Route("/create", name="shortCreate", methods={"POST"})
+     */
+    public function create(Request $request, EntityManagerInterface $manager, SerializerInterface $serializer): Response
+    {
+        $data = $request->getContent();
+
+        $short = $serializer->deserialize($data, Short::class, 'json');
+
+        $manager->persist($short);
+        $manager->flush();
+
+        return $this->json($short);
+    }
+
+    /**
+     * @Route("/delete/{id}", name="shortDelete")
+     */
+    public function delete(Short $short, EntityManagerInterface $manager): Response
+    {
+        $manager->remove($short);
+        $manager->flush();
+
+        return $this->json("REMOVE OK");
+    }
+
 }
